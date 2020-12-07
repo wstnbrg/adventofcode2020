e5bbb88d69adfbc7495ecfb93ded039ad91bdd81
Henning Westenberg <westenberg@hucke-media.de>
2020-12-07 17:26:42 +0100

    day 05 - part 1

---
+++ 05/index.js
@@ -0,0 +1,73 @@
+const fs = require('fs');
+const { setUncaughtExceptionCaptureCallback } = require('process');
+
+var dataArray = [];
+var seatIds = [];
+
+try {
+    const data = fs.readFileSync('input.txt', 'UTF-8');
+    const lines = data.split(/\r?\n/);
+
+    lines.forEach((line) => {
+        dataArray.push(line);
+    });
+} catch (err) {
+    console.error(err);
+}
+
+dataArray.forEach((line) => {
+    var seatPos = line.split('');
+    var seatRow;
+    var seatCol;
+    var range = {
+        min: 0,
+        max: 127
+    }
+
+    seatPos.forEach((rangeHalf, index) => {
+        if (index < 7) {
+            range = checkHalf(rangeHalf, range);
+        }
+        if (index == 6) {
+            seatRow = range.min;
+            range = {
+                min: 0,
+                max: 7
+            }
+        }
+        if (index > 6) {
+            range = checkHalf(rangeHalf, range);
+        }
+    });
+    
+    seatCol = range.min;
+
+    var seatId = seatRow * 8 + seatCol;
+
+    seatIds.push(seatId);
+});
+
+var highestSeatId = 0;
+
+seatIds.forEach((seat) => {
+    if (seat > highestSeatId) {
+        highestSeatId = seat;
+    }
+});
+
+console.log('Answer: ', highestSeatId);
+
+function checkHalf(halfIndikator, range) {
+    half = parseInt(((range.max +1) - range.min) / 2);
+    if (halfIndikator == 'F' ||
+        halfIndikator == 'L'
+    ) {
+        range.max = range.max - half;
+    }
+    if (halfIndikator == 'B' ||
+        halfIndikator == 'R'
+    ) {
+        range.min = (range.max + 1) - half;
+    }
+    return range;
+}
