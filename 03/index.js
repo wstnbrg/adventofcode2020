c56da3b79f909b3ab599a49767814f2a035ddc21
Henning Westenberg <westenberg@hucke-media.de>
2020-12-07 11:21:14 +0100

    day 03 - part 2

--- 03/index.js
+++ 03/index.js
@@ -1,49 +1,78 @@
 const fs = require('fs');
 
-var map = [];
-var treesEncountered = 0;
-var currentPos = {
-    left: 0,
-    top: 0
-};
+var treesPerRun = [];
+var runsRight = [
+    1,
+    3,
+    5,
+    7,
+    1
+];
+var runsDown = [
+    1,
+    1,
+    1,
+    1,
+    2
+];
 
-try {
-    const data = fs.readFileSync('input.txt', 'UTF-8');
-    const lines = data.split(/\r?\n/);
-    var tmpLeft = currentPos.left;
-
-    lines.forEach((line) => {
-        // # = tree
-        // . = square
-        column = line.split('');
-        tmpLeft += 3;
-
-        if (line.length / tmpLeft < 1)
-        {
-            for (let index = 0; index < (tmpLeft - line.length); index++) {
-                column.push(column[index]);        
+runsRight.forEach((spacing, index) => {
+    var down = index;
+    var map = [];
+    var treesEncountered = 0;
+    var currentPos = {
+        left: 0,
+        top: 0
+    };
+    
+    try {
+        const data = fs.readFileSync('input.txt', 'UTF-8');
+        const lines = data.split(/\r?\n/);
+        var tmpLeft = currentPos.left;
+    
+        lines.forEach((line) => {
+            // # = tree
+            // . = square
+            column = line.split('');
+            tmpLeft += spacing;
+    
+            if (line.length / tmpLeft < 1)
+            {
+                for (let index = 0; index < (tmpLeft - line.length); index++) {
+                    column.push(column[index]);        
+                }
             }
+    
+            map.push(column);
+        });
+    } catch (err) {
+        console.error(err);
+    }
+    map.forEach((row, index) => {
+        if (index % runsDown[down] === 0) {
+            var nextPos = {
+                left: currentPos.left + spacing,
+                top: currentPos.top + runsDown[down]
+            }
+        
+            if (row[currentPos.left] == '#')
+            {
+                treesEncountered++;
+            }
+        
+            currentPos.left = nextPos.left;
+            currentPos.top = nextPos.top;
         }
-
-        map.push(column);
     });
-} catch (err) {
-    console.error(err);
-}
-
-map.forEach((row, index) => {
-    var nextPos = {
-        left: currentPos.left + 3,
-        top: currentPos.top + 1
-    }
-
-    if (row[currentPos.left] == '#')
-    {
-        treesEncountered++;
-    }
-
-    currentPos.left = nextPos.left;
-    currentPos.top = nextPos.top;
+    treesPerRun.push(treesEncountered);
 });
 
-console.log('encountered trees: ', treesEncountered);
+var answer = 1;
+
+console.log(treesPerRun);
+
+treesPerRun.forEach((trees) => {
+    answer *= trees;
+});
+
+console.log('encountered trees: ', answer);
